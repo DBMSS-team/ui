6428d91f8ff85d981b052e006be854849571a244
Sreedevi <sreedevi.madala@sap.com>
2020-09-09 20:45:36 +0530

    home page aligned

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -51,44 +51,46 @@ export default function Album() {
 				</div>
 				<LocAutoComp />
 			</div>
-			<main>
-				<Container className="card__grid">
-					<div class="home-title">What do you want?</div>
-					<div>
-						<hr></hr>
-					</div>
-					{/* End hero unit */}
-					<Link to="/Store" style={{ textDecoration: "none" }}>
-						<Grid container spacing={4}>
-							{cards.map((card) => (
-								<Grid item key={card} xs={12} sm={6} md={4}>
-									<Card className="root">
-										<div className="details">
-											<CardContent className="content">
-												<div class="card-image">
-													<img src="/images/delivery.svg"></img>
-												</div>
-												<Typography
-													component="h5"
-													variant="h5"
-													class="home-card-detail"
-												>
-													{card}
-												</Typography>
-											</CardContent>
-											<div className="controls"></div>
-										</div>
-										<CardMedia
-											className="cover"
-											image="https://source.unsplash.com/random"
-											title="{code}"
-										/>
-									</Card>
-								</Grid>
-							))}
-						</Grid>
-					</Link>
-				</Container>
+			<main className="card__main">
+				<div className="card__sub__main">
+					<Container className="card__grid">
+						<div class="home-title">What do you want?</div>
+						<div>
+							<hr></hr>
+						</div>
+						{/* End hero unit */}
+						<Link to="/Store" style={{ textDecoration: "none" }}>
+							<Grid container spacing={4}>
+								{cards.map((card) => (
+									<Grid item key={card} xs={12} sm={6} md={4}>
+										<Card className="root">
+											<div className="details">
+												<CardContent className="content">
+													<div class="card-image">
+														<img src="/images/delivery.svg"></img>
+													</div>
+													<Typography
+														component="h5"
+														variant="h5"
+														class="home-card-detail"
+													>
+														{card}
+													</Typography>
+												</CardContent>
+												<div className="controls"></div>
+											</div>
+											<CardMedia
+												className="cover"
+												image="https://source.unsplash.com/random"
+												title="{code}"
+											/>
+										</Card>
+									</Grid>
+								))}
+							</Grid>
+						</Link>
+					</Container>
+				</div>
 			</main>
 			<Footer />
 		</div>
