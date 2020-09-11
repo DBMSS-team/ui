ada6b08943751ef1b6739292f64a39dc712c643a
deepakbharadwaj <32411411+deepakbharadwaj@users.noreply.github.com>
2020-09-11 16:35:43 +0530

    logic to store location details added

--- src/components/LocAutoComp.jsx
+++ src/components/LocAutoComp.jsx
@@ -1,47 +1,46 @@
-import React, {
-	useState
-} from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import {
-	config
-} from "../config";
-import SearchBar from 'material-ui-search-bar';
-import Script from 'react-load-script';
-import '../styles/LocAutoComp.css';
+import { config } from "../config";
+import SearchBar from "material-ui-search-bar";
+import Script from "react-load-script";
+import "../styles/LocAutoComp.css";
+require("dotenv").config();
 
-export default function LocAutoComp({
-	data
-}) {
-
+export default function LocAutoComp({ data }) {
 	const [state, setState] = useState({
-		city: '',
-		query: '',
+		city: "",
+		query: "",
 	});
+
+	const gmapsUrl = process.env.GMAPS_API_URL;
 
 	function handleScriptLoad() {
 		// Declare Options For Autocomplete
 		const options = {
 			types: [],
-		};
+		}; // To disable any eslint 'google not defined' errors
 
 		// Initialize Google Autocomplete
-		/*global google*/ // To disable any eslint 'google not defined' errors
-		const autocomplete = new google.maps.places.Autocomplete(
-			document.getElementById('autocomplete'),
-			options,
+		/*global google*/ const autocomplete = new google.maps.places.Autocomplete(
+			document.getElementById("autocomplete"),
+			options
 		);
 
 		// Avoid paying for data that you don't need by restricting the set of
 		// place fields that are returned to just the address components and formatted
 		// address.
-		autocomplete.setFields(['address_components', 'formatted_address', "geometry", "icon", "name"]);
+		autocomplete.setFields([
+			"address_components",
+			"formatted_address",
+			"geometry",
+			"icon",
+			"name",
+		]);
 
 		// Fire Event when a suggested name is selected
-		autocomplete.addListener('place_changed', () => {
+		autocomplete.addListener("place_changed", () => {
 			// Extract City From Address Object
 			const addressObject = autocomplete.getPlace();
-			const la = addressObject.geometry.location.lat();
-			const lo = addressObject.geometry.location.lng();
 			const address = addressObject.address_components;
 
 			// Check if address is valid
@@ -52,13 +51,15 @@ export default function LocAutoComp({
 					query: addressObject.formatted_address,
 				});
 			}
-			handleSubmit(la)
+			handleSubmit(addressObject);
 		});
 	}
 
-	function handleSubmit(la) {
+	function handleSubmit(addressObject) {
 		var payload = {
-			location: la
+			latitude: addressObject.geometry.location.lat(),
+			longitude: addressObject.geometry.location.lng(),
+			place: addressObject.address_components[0].long_name,
 		};
 		axios
 			.post(config.SERVER_URL + "/location", payload)
@@ -75,22 +76,19 @@ export default function LocAutoComp({
 			});
 	}
 
-	return ( 
-		<div className = "LocAutoComp">
-		<Script url = "https://maps.googleapis.com/maps/api/js?key=&libraries=places" onLoad = {handleScriptLoad}/> 
-		<div className = "Search__bar" >
-		<SearchBar className = "Search__box"
-		id = "autocomplete"
-		placeholder = "Search for a location"
-		hintText = "Search City"
-		value = {
-			state.query
-		}
-		/> 
-		<button className = "Search__button" >
-		Proceed 
-		</button> 
-		</div> 
+	return (
+		<div className="LocAutoComp">
+			<Script url={gmapsUrl} onLoad={handleScriptLoad} />
+			<div className="Search__bar">
+				<SearchBar
+					className="Search__box"
+					id="autocomplete"
+					placeholder="Search for a location"
+					hintText="Search City"
+					value={state.query}
+				/>
+				<button className="Search__button">Proceed</button>
+			</div>
 		</div>
 	);
 }
